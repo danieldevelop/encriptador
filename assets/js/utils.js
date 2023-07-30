66c9142a74692e0934d276c9f5b4e4b52fcb3c63
Daniel Gomez <dgomez.informatica@gmail.com>
2023-07-30 12:33:31 -0400

    funciones generales para todo el proyecto

---
+++ assets/js/utils.js
@@ -0,0 +1,18 @@
+const regexText = new RegExp(/^[a-zñ ]+$/gm);
+
+export const esTexto = (texto) => (texto.match(regexText) ? true : false);
+
+export const miniAlerta = (mensaje, icono, boton, position) => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: position,
+        showConfirmButton: boton,
+        showCancelButton: boton,
+        timer: 3000,
+    });
+
+    Toast.fire({
+        icon: icono,
+        title: mensaje,
+    });
+}
