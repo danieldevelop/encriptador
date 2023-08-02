97544a8e7eaf4a61711bd14cd10f571342832f09
Daniel Gomez <dgomez.informatica@gmail.com>
2023-08-02 00:11:55 -0400

    add funcionalidad para encriptar el texto

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
-import { 
-    miniAlerta, 
-    esTexto } 
-from "./utils.js";
+import { miniAlerta, esTexto, esconderElemento } from "./utils.js";
+import { encripta, desencripta, copiarTexto } from "./funciones.js";
 
 const btnEncriptar = document.getElementById('btn-encriptar');
 const brnDesencriptar = document.getElementById('btn-desencriptar');
+const textoProcesado = document.getElementById('texto-procesado');
+const btnCopiarTexto = document.getElementById('btn-copiar-texto');
 
 btnEncriptar.addEventListener('click', () => {
     const inpTexto = document.getElementById('texto').value.trim();
@@ -16,5 +16,17 @@ btnEncriptar.addEventListener('click', () => {
         miniAlerta('Debes ingresar solo letras minusculas', 'error', false, 'top');
         return;
     }
+
+    // Modificamos los estilos de los elementos
+    esconderElemento('.img-munheco');
+    esconderElemento('.informacion__info');
+    esconderElemento('.informacion__leyenda');
+    textoProcesado.style.display = 'block';
+    textoProcesado.style.height = '100%';
+    // Mostramos el texto procesado en el txtarea encriptado
+    textoProcesado.value = encripta(inpTexto);
+    // Mostramos el boton de copiar texto
+    btnCopiarTexto.style.display = 'block';
+
 });
 
